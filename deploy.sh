709a7be01fda4a6ff1df4a2fe0fa6f9ec49dddc7
Brian Trang <briantrang@ymail.com>
2022-04-16 21:06:02 -0700

    Deploy final update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,13 @@
+# Generate latest build files in case you forget
+echo "Generating build files in case it was forgotten"
+npm run build
+
+# Commit strangling build files
+echo "Commiting strangling build files"
+git add .
+git commit -m "Commiting build files"
+git push
+
 # Script to re push to github
 echo "Copying build folder to temp parent folder"
 cp -r build ../build
